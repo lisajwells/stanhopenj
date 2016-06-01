c1e09817716820fc37889d51bf58230527b0cc8d
Lisa Wells <lisawells999@gmail.com>
2016-06-01 07:13:06 -0400

    front news: get_the_excerpt

--- front-page.php
+++ front-page.php
@@ -42,15 +42,9 @@ function stanhopenj_news_loop() {
     if( $loop->have_posts() ) {
         // loop through posts
         while( $loop->have_posts() ): $loop->the_post();
-        $video_id = esc_attr( genesis_get_custom_field( 'cd_youtube_id' ) );
-        $video_thumbnail = '<img src="http://img.youtube.com/vi/' . $video_id . '/0.jpg" alt="" />';
-        $video_link = 'http://www.youtube.com/watch?v=' . $video_id;
-        echo '<div class="one-third first">';
-            echo '<a href="' . $video_link . '" target="_blank">' . $video_thumbnail . '</a>';
-        echo '</div>';
-        echo '<div class="two-thirds">';
+        echo '<div class="frontpage-news-item">';
             echo '<h4>' . get_the_title() . '</h4>';
-            echo '<p>' . get_the_date() . '</p>';
+            echo '<p>' . get_the_excerpt() . '</p>';
             echo '<p><a href="' . $video_link . '" target="_blank">Watch It</a> | <a href="' . get_permalink() . '" target="_blank">Show Notes</a></p>';
         echo '</div>';
         endwhile;
