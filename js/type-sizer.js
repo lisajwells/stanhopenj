fdf6d33b7f5333434631e7e1671b9d86447da6b8
Lisa Wells <lisawells999@gmail.com>
2016-06-02 15:11:51 -0400

    css for <p>s of type-sizer

--- js/type-sizer.js
+++ js/type-sizer.js
@@ -1,34 +1,35 @@
 jQuery(function( $ ){
 
 
-//***** type-size experiment *****//
-//***** requires jquery.cookie.js *****//
+  //***** type-sizer *****//
+  //***** requires jquery.cookie.js *****//
   var storedTextSize = $.cookie('textSizeCookie');
 
-  // set data-type-size attribute of parent to match attribute stored in cookie
-  $('.entry-content p').attr('data-type-size', storedTextSize);
-
-  // MARK .SELECTED T
-  // if no cookie has been set, lilT gets .selected class
-  if ( typeof storedTextSize == "undefined" ) {
-    $('a.lilT').addClass('selected');
-  }
-
-  else { $('a.'+storedTextSize).addClass('selected');
-  }
-
-  $('.tees a').click(function() {
-    // set a cookie with the data-type-size attribute of the clicked <a>
-    textSize = $(this).attr('data-type-size');
-    $.cookie('textSizeCookie', textSize, { expires: 9000, path: '/' } ); //days
-
-    // change data-type-size attribute of parent to match attribute of clicked <a>
-    $('.entry-content p').attr('data-type-size', textSize);
+    // set data-type-size attribute of <p> to match attribute stored in cookie
+    $('.entry-content p').attr('data-type-size', storedTextSize);
 
     // mark .selected T
-    $('.tees a').removeClass('selected');
-    $(this).addClass('selected');
+    // if no cookie has been set, lilT gets .selected class
+    if ( typeof storedTextSize == "undefined" ) {
+      $('a.lilT').addClass('selected');
+    }
+
+    else { $('a.'+storedTextSize).addClass('selected');
+    }
+
+    $('.tees a').click(function() {
+      // set a cookie with the data-type-size attribute of the clicked <a>
+      textSize = $(this).attr('data-type-size');
+      $.cookie('textSizeCookie', textSize, { expires: 9000, path: '/' } ); //days
+
+      // change data-type-size attribute of <p> to match attribute of clicked <a>
+      $('.entry-content p').attr('data-type-size', textSize);
+
+      // mark .selected T
+      $('.tees a').removeClass('selected');
+      $(this).addClass('selected');
   });
+
   //**//
 
 });
