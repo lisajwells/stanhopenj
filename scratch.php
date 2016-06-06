75ad9d52365e03480b426fad5cafe62b762c1b03
Lisa Wells <lisawells999@gmail.com>
2016-06-06 15:53:57 -0400

    scratch from html to php

---
+++ scratch.php
@@ -0,0 +1,24 @@
+<div class="members-box">
+<h4>Members</h4>
+<ul class="members-list">
+  <li><strong></strong></li>
+  <li></li>
+</ul>
+</div>
+
+<a href="tel:000-000-0000">000-000-0000</a>
+
+<li>Doreen Thistleton <span class="member-box-info">(Term expires 12/31/2016)</span></li>
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+<?php
+
+is_page('government') || is_page('mayor-and-council') || is_page('council-meetings-agendas-and-minutes') || is_category('minutes-board-of-health') || is_single() && in_category('minutes-board-of-health') || is_category('minutes-environmental-commission') || is_single() && in_category('minutes-environmental-commission') || is_category('minutes-land-use-board') || is_single() && in_category('minutes-land-use-board') || is_category('minutes-mayor-and-council') || is_single() && in_category('minutes-mayor-and-council') || is_category('minutes-recreation-commission') || is_single() && in_category('minutes-recreation-commission') || is_category('minutes-shade-tree-commission') || is_single() && in_category('minutes-shade-tree-commission')
+
+
+is_page(array('government', 'mayor-and-council', 'council-meetings-agendas-and-minutes')) || is_category(array('minutes-board-of-health', 'minutes-environmental-commission', 'minutes-land-use-board', 'minutes-mayor-and-council', 'minutes-recreation-commission', 'minutes-shade-tree-commission' ))
+
+is_single() && in_category('baked-goods')
+
+is_category('minutes-board-of-health') || is_single() && in_category('minutes-board-of-health') || is_category('minutes-environmental-commission') || is_single() && in_category('minutes-environmental-commission') || is_category('minutes-land-use-board') || is_single() && in_category('minutes-land-use-board') || is_category('minutes-mayor-and-council') || is_single() && in_category('minutes-mayor-and-council') || is_category('minutes-recreation-commission') || is_single() && in_category('minutes-recreation-commission') || is_category('minutes-shade-tree-commission') || is_single() && in_category('minutes-shade-tree-commission')
