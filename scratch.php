273aad1c8aa0afd7a15976940a3b46ba9f249c66
Lisa Wells <lisawells999@gmail.com>
2016-06-06 22:20:55 -0400

    pulled from SG

--- scratch.php
+++ scratch.php
@@ -17,9 +17,7 @@
 is_page('government') || is_page('mayor-and-council') || is_page('council-meetings-agendas-and-minutes') || is_category('minutes-board-of-health') || is_single() && in_category('minutes-board-of-health') || is_category('minutes-environmental-commission') || is_single() && in_category('minutes-environmental-commission') || is_category('minutes-land-use-board') || is_single() && in_category('minutes-land-use-board') || is_category('minutes-mayor-and-council') || is_single() && in_category('minutes-mayor-and-council') || is_category('minutes-recreation-commission') || is_single() && in_category('minutes-recreation-commission') || is_category('minutes-shade-tree-commission') || is_single() && in_category('minutes-shade-tree-commission')
 
 
-is_page(array('government', 'mayor-and-council', 'council-meetings-agendas-and-minutes')) ||
-
-is_category(array('minutes-board-of-health', 'minutes-environmental-commission', 'minutes-land-use-board', 'minutes-mayor-and-council', 'minutes-recreation-commission', 'minutes-shade-tree-commission' ))
+is_page(array('government', 'mayor-and-council', 'council-meetings-agendas-and-minutes')) || is_category(array('minutes-board-of-health', 'minutes-environmental-commission', 'minutes-land-use-board', 'minutes-mayor-and-council', 'minutes-recreation-commission', 'minutes-shade-tree-commission' ))
 
 is_single() && in_category('baked-goods')
 
